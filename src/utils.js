8ebb223208dd51be7872068701af51e987e988e6
fess986u <fess986@mail.ru>
2023-08-06 12:10:22 +0300

    Установить линтер в проект #20
    - установлен eslint
    - установлен когфиг airbnb для линтера
    - исправлены все ошибки, который выдал линтер
    - подредактирован файл конфига .eslintrc.json чтобы убрать странные
    а иногда и вовсе глючные предупреждения

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,18 @@
-import { unlink } from 'fs/promises'
-import fs from 'fs'
+import { unlink } from 'fs/promises';
+import fs from 'fs';
 
 // функция для удаления файла по его пути в файловой системе
 export default async function removeFile(path) {
   try {
     unlink(path);
-  } catch(error) {
-    console.log('error deleting file', error.message)
+  } catch (error) {
+    console.log('error deleting file', error.message);
   }
 }
 
 export function deleteFolderRecursive(path) {
   if (fs.existsSync(path)) {
-    fs.readdirSync(path).forEach(file => {
+    fs.readdirSync(path).forEach((file) => {
       const curPath = `${path}/${file}`;
       if (fs.lstatSync(curPath).isDirectory()) {
         // рекурсивно вызываем функцию для удаления содержимого папки
