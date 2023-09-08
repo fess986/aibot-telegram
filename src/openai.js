95f6f517ae5b05dbc79e80031a69145a7cb2f20d
fess986u <fess986@mail.ru>
2023-09-08 17:49:37 +0300

    Апгрейдим версию openai до 4.5 #41
    - меняем версию в package.json
    - обновляем node_modules через npm i
    - инициализируем openai по новым стандартам
    - меняем обращение к текстовому чату

--- src/openai.js
+++ src/openai.js
@@ -1,15 +1,13 @@
 import { createReadStream } from 'fs';
-import { Configuration, OpenAIApi } from 'openai';
+// import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 import config from 'config';
 
-class OpenAI {
+class OpenAIClass {
   constructor(apiKey) {
-    const configuration = new Configuration({
-      apiKey,
-      models: ['davinci'],
-      temperature: 0.5,
+    this.openai = new OpenAI({
+      apiKey  // This is also the default, can be omitted
     });
-    this.openai = new OpenAIApi(configuration);
   }
 
   async transcription(filePath) {
@@ -50,7 +48,7 @@ class OpenAI {
         setTimeout(() => resolve('ошибка'), 80000);
       });
 
-      const responsePromise = this.openai.createChatCompletion({
+      const responsePromise = this.openai.chat.completions.create({
         model: 'gpt-3.5-turbo', // модель. в будущем будет доступна еще версия с 4 чатом
         messages, // заданный массив запроса, где кроме самого запроса еще есть роль, контекст и тд
       });
@@ -58,11 +56,10 @@ class OpenAI {
       // ждем ответа от чата.
       const response = await Promise.race([responsePromise, timePromise]);
 
-      const responseText = typeof response === 'string' ? 'ошибка' : response.data.choices[0].message;
+      const responseText = typeof response === 'string' ? 'ошибка' : response.choices[0].message;
 
       return responseText;
     } catch (err) {
-      // await ctx.reply('Ошибка ответа от чата-аи, текст ошибки: ', err.message)
       console.log('error chating with gpt', err.message);
       return null; // Возвращаем null или другое значение по умолчанию в случае ошибки
     }
@@ -125,4 +122,4 @@ class OpenAI {
   }
 }
 
-export const openAi = new OpenAI(config.get('OPENAI_KEY'));
+export const openAi = new OpenAIClass(config.get('OPENAI_KEY'));
